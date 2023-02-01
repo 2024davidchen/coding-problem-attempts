8e50a2846fc2eaa297a6d3d7a8a8f7639f977dba
2024davidchen <84689232+2024davidchen@users.noreply.github.com>
2023-02-01 13:58:46 -0500

    Verified compile

--- kattis/2048.py
+++ kattis/2048.py
@@ -24,29 +24,42 @@ def move(numList):
     return numList
 
 def compile(numList):
-    for i in range(4):
+    for i in range(3):
         move(numList)
-        if numList[i] == numList[i + 1]:
+        move(numList)
+        move(numList)
+        move(numList)
+        if numList[i] == numList[i + 1] and numList[i] != 0:
             numList[i] *= 2
             numList[i + 1] = 0
             numList = move(numList)
-            numLIst = compile(numList)
     return numList
 
 
 def left(gameBoard):
+    boardCopy = gameBoard
     for i in range(4):
         gameBoard[i] = compile(gameBoard[i])
-    return gameBoard
+    return boardCopy
 
 def up(gameBoard):
     for i in range(4):
         column1, column2, column3, column4 = [0, 0, 0, 0]
         for j in range(4):
+            pass
 
 
 def right(gameBoard):
-
+    pass
 
 def down(gameBoard):
+    pass
 
+
+if __name__ == '__main__':
+    line1 = input()
+    line2 = input()
+    line3 = input()
+    line4 = input()
+    board = makeBoard(line1, line2, line3, line4)
+    print(left(board))
